a5e56caac25a01e48c10449b689cbb194a5ea807
ozzy onwuzu <ozonniaonwuzu@mail.adelphi.edu>
2021-06-23 07:20:09 -0700

    add modified scripts.js page

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
 function add(number1, number2) {
   return number1 + number2;
 }
-
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
 alert(add(number1, number2));
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+alert(subtract(number1, number2));
+
+function multiply(number1, number2) {
+  return number1 x number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+alert(multiply(number1, number2));
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+alert(divide(number1, number2));
